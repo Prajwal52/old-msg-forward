c36f26adac6ce21b0b444d1630429bc0852ceef6
Prajwal52 <134858777+Prajwal52@users.noreply.github.com>
2023-12-14 17:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste1")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'forward_files')
     SESSION = os.environ.get("SESSION", "BQFIc8EAUSQDdKcjBhiZSxpYKqNNBL2oLyjLG9L9rBhHowjycxTfoLDo6f0-F_IUUnYM8vUw9Qw2sbEIU-UIBfJdJ3kRpItSctSEM1bPxrJdCno9Lnz0bh_0uqJrlkeBed9yJx3GBESXpM0uvDU7nZulUv-Nj75DG8mX3pZ18c7-QlveQ5jzcrC10HXxHalfpM3d95RjFBwYtn8eiKtwlq3WnDCeeavn_7S5pp0Db4tdwzSwLm5H1N1exKpLoExE4AfsDqN6W9Ya15g5LE98r-DWAOzZl2M-uUUBKyMPjjEvXd7gzuPVhvXqJqwrVPquKtBU9wnPTGO5Hu199NBC8H_5qPHxfwAAAABF5vF9AA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001927033569"))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1002127116469"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "@nish_27_bot")
 
 
